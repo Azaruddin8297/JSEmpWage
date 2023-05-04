867605555c49d315128b95b03da6ddcf580a52a7
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-05-04 20:20:55 +0530

    EmpWages

--- Employee.js
+++ Employee.js
@@ -1,12 +1,23 @@
-//UC1 
-const IS_PRESENT = 0;
-let empcheck = Math.floor(Math.random() * 10) % 2;
-if(empcheck==IS_PRESENT)
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 0;
+const NUM_OF_WORKINGDAYS = 20;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const RATE_PER_HOUR = 20;
+
+let empcheck = Math.floor(Math.random() * 10) % 3;
+
+function getWorkingHours(empcheck)
 {
-    console.log("Present");
-    return;   
+switch(empcheck){
+    case IS_FULL_TIME :
+        return FULL_TIME_HOURS;
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            default:
+                return 0;
 }
-else
-{
-    console.log("Absent")
 }
+let emphrs = getWorkingHours(empcheck);
+let empwages = emphrs * RATE_PER_HOUR;
+console.log("Total Emp Wages "+empwages);
