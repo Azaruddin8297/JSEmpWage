aac587442baead995c8d1646c7c9fbb4a96f7e18
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-05-04 20:22:31 +0530

    Refactor

--- Employee.js
+++ Employee.js
@@ -1,9 +1,9 @@
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 0;
-const NUM_OF_WORKINGDAYS = 20;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const RATE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 let empcheck = Math.floor(Math.random() * 10) % 3;
 
@@ -18,6 +18,12 @@ switch(empcheck){
                 return 0;
 }
 }
-let emphrs = getWorkingHours(empcheck);
+let emphrs = 0;
+for(let day = 0; day<NUM_OF_WORKING_DAYS;day++){
+   
+     emphrs = emphrs + getWorkingHours(empcheck);
+    
+    
+}
 let empwages = emphrs * RATE_PER_HOUR;
-console.log("Total Emp Wages "+empwages);
+console.log("Monthly Emp Wages "+empwages);
